036d5c1d329548678491d858b06e18cd3991455a
Naranut05 <naranut.sresroy@gmail.com>
2025-07-22 14:29:51 +0700

    Assignment

--- ex-1.js
+++ ex-1.js
@@ -3,24 +3,33 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary + 5000;
 }
 
 function forEach(array, operation) {
   // Start coding here
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    const newSalary = operation(array[i]);
+    result.push(newSalary);
+  }
+  return result;
 }
 
 // Using `forEach` function here
 
 let newEmployeeSalaries;
 
+console.log(newEmployeeSalaries)= forEach(employeeSalaries, addSalary5000);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
 /*
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+// addSalary5000
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
-
+// function forEach(array, operation)
 ====================================
 */
